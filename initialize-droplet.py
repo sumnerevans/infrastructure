817f491b25e6ced2a12a74b6fd8736c5c1a362f6
Sumner Evans <sumner.evans98@gmail.com>
2020-03-23 22:34:37 -0600

    Added host.nix

--- initialize-droplet.py
+++ initialize-droplet.py
@@ -111,7 +111,7 @@ user_data = '''#cloud-config
 runcmd:
   - apt install -y git
   - git clone https://gitlab.com/sumner/infrastructure.git /etc/nixos
-  - curl https://raw.githubusercontent.com/elitak/nixos-infect/master/nixos-infect | PROVIDER=digitalocean NIX_CHANNEL=nixos-19.09 bash 2>&1 | tee /tmp/infect.log
+  - curl https://raw.githubusercontent.com/elitak/nixos-infect/master/nixos-infect | PROVIDER=digitalocean NIXOS_IMPORT=./host.nix NIX_CHANNEL=nixos-20.03 bash 2>&1 | tee /tmp/infect.log
 '''
 
 print()
@@ -139,6 +139,7 @@ droplet = digitalocean.Droplet(
     image=image,
     ipv6=True,
     name=name,
+    private_networking=True,
     region=region,
     size_slug=size,
     ssh_keys=keys_to_use,
