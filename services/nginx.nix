f102b48dd0eb2214b502a129cf8d32ace999a895
Sumner Evans <sumner.evans98@gmail.com>
2020-03-24 12:11:21 -0600

    Trying to get website forwarding

--- services/nginx.nix
+++ services/nginx.nix
@@ -1,4 +1,4 @@
-{pkgs, services, ...}: {
+{ pkgs, config, ... }: {
   services.nginx = {
     enable = true;
     enableReload = true;
@@ -7,11 +7,20 @@
     recommendedProxySettings = true;
     recommendedTlsSettings = true;
 
-    # Enable a status page and expose it.
-    virtualHosts."status.sumnerevans.com" = {
-      forceSSL= true;
-      enableACME = true;
-      locations."/".extraConfig = "stub_status on;access_log off;";
+    virtualHosts = {
+      # Enable a status page and expose it.
+      "status.sumnerevans.com" = {
+        forceSSL= true;
+        enableACME = true;
+        locations."/".extraConfig = "stub_status on;access_log off;";
+      };
+
+      # By default, proxy requests to sumnerevans.com to GitLab.
+      "${config.networking.domain}" = {
+        locations."/" = {
+          proxyPass = "https://35.185.44.232";
+        };
+      };
     };
   };
 }
