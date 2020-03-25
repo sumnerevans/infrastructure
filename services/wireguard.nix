694094491877a228fa3bd8c61c1197eab3999b7f
Sumner Evans <sumner.evans98@gmail.com>
2020-03-25 01:01:26 -0600

    Fixed wireguard config

--- services/wireguard.nix
+++ services/wireguard.nix
@@ -1,34 +1,40 @@
 { pkgs, ... }: {
-  networking.wg-quick.interfaces.wg0 = {
-    address = [ "10.100.100.1/24" ];
+  networking.wireguard.enable = true;
+  networking.wireguard.interfaces.wg0 = {
+    ips = [ "192.168.69.1/24" ];
+    listenPort = 51820;
     privateKeyFile = "/etc/nixos/secrets/wireguard-privatekey";
+
     peers = [
       {
         # jedha
         allowedIPs = [ "192.168.69.2/32" ];
-        publicKey = "x1BDx/V7ylfF8lKr5ZbYH+XR3UjJG4M0VZTBJTz1Jmo=";
         presharedKeyFile = "/etc/nixos/secrets/wireguard-jedha-presharedkey";
+        publicKey = "x1BDx/V7ylfF8lKr5ZbYH+XR3UjJG4M0VZTBJTz1Jmo=";
       }
       {
         # coruscant
         allowedIPs = [ "192.168.69.3/32" ];
-        publicKey = "z2sm3HzY+9+7+lT9rt2Ny7fTxbDVMJv4+jZ3eFlyUCc=";
         presharedKeyFile = "/etc/nixos/secrets/wireguard-coruscant-presharedkey";
+        publicKey = "z2sm3HzY+9+7+lT9rt2Ny7fTxbDVMJv4+jZ3eFlyUCc=";
       }
       {
         # iPad
         allowedIPs = [ "192.168.69.4/32" ];
-        publicKey = "4qHoCiAVYm2kR6Ak4PaEpdUeRJ/oJDlVQvR8Axffv2U=";
         presharedKeyFile = "/etc/nixos/secrets/wireguard-ipad-presharedkey";
+        publicKey = "4qHoCiAVYm2kR6Ak4PaEpdUeRJ/oJDlVQvR8Axffv2U=";
       }
       {
         # Google Pixel 3a
         allowedIPs = [ "192.168.69.5/32" ];
-        publicKey = "MGeD4bj63h/EAvI3EYGM3kyQ0mh0Srxv02pTTVl5KwY=";
         presharedKeyFile = "/etc/nixos/secrets/wireguard-pixel-presharedkey";
+        publicKey = "MGeD4bj63h/EAvI3EYGM3kyQ0mh0Srxv02pTTVl5KwY=";
       }
     ];
   };
+
+  # Open up the ports
+  networking.firewall.allowedTCPPorts = [ 51820 ];
 
   # TODO run unbound
 }
