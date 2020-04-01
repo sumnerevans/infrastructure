21156ef0a7061196bc7f9d6706a511435d9379a5
Sumner Evans <me@sumnerevans.com>
2020-04-01 16:50:38 -0600

    Try to get cert for quassel

--- services/mumble.nix
+++ services/mumble.nix
@@ -20,7 +20,6 @@ in {
     allowedUDPPorts = [ 64738 ];
   };
 
-  # TODO this certs stuff doesn't work
   # Use nginx to do the ACME verification for mumble.
   services.nginx.virtualHosts."${serverName}" = {
     locations."/".extraConfig = "return 301 https://mumble.info;";
--- services/quassel.nix
+++ services/quassel.nix
@@ -1,8 +1,24 @@
-{ pkgs, ... }: {
+{ config, pkgs, ... }: let
+  certs = config.security.acme.certs;
+  serverName = "irc.sumnerevans.com";
+in {
   services.quassel = {
     enable = true;
     interfaces = [ "0.0.0.0" ];
+    certificateFile = "${certs.${serverName}.directory}/fullchain.pem";
   };
 
   networking.firewall.allowedTCPPorts = [ 4242 ];
+
+  # Use nginx to do the ACME verification for mumble.
+  services.nginx.virtualHosts."${serverName}" = {
+    locations."/".extraConfig = "return 301 https://https://quassel-irc.org;";
+    locations."/.well-known/acme-challenge".root = "/var/lib/acme/acme-challenges";
+  };
+
+  security.acme.certs."${serverName}" = {
+    webroot = "/var/lib/acme/acme-challenges";
+    postRun = "systemctl restart quassel";
+    user = "quassel";
+  };
 }
