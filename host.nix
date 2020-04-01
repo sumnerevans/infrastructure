32dcfce8cd1acc6972052fce6ae2b5b06df4dbe5
Sumner Evans <me@sumnerevans.com>
2020-04-01 12:58:22 -0600

    Add neovim

--- host.nix
+++ host.nix
@@ -7,6 +7,7 @@
     fd
     git
     htop
+    neovim
     openssl
     ripgrep
     tmux
@@ -51,5 +52,5 @@
     openssh.authorizedKeys.keys = [
       "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCs0CbfyzxbTTST4bYVZ4qhV8WQR1EWDRlzhaX4MfCGT3DyokXSfhe+RWdvo2FGFwduFMkVEKTGbMCkdt7Ip3vNYuWNB36oimEV9zB37ejD6wPZcEem/P9PR0gb0Cy/XuMkBhXaeA+vPSGU9WRBOuVuFQQRX+NoC62KTwmZac1ro9nx4bMa2OYDnDNh2ogSXVkHGutpP+iUnESTA3d2fB1j9x+wbDRmDQvrYKdlC8mNeSuzDd/1KL0eDI+Y2rmdKZ+QZW/E2Y41l7AI7IOG2i1Y+aS8JkhUjZmO9Ci3ApMHbGtL6X42oQ+TxDIQVBq/GKEbWLigsp1WlqeEzqA+GbOp GitLab-CD"
     ];
-  }
+  };
 }
