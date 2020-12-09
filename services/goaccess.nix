a428e7293f61c60452fda06de85c3ee274e6ec1b
Sumner Evans <me@sumnerevans.com>
2020-12-09 13:27:06 -0700

    Goaccess improvement

--- services/goaccess.nix
+++ services/goaccess.nix
@@ -6,32 +6,60 @@
     "184.96.89.215"
   ];
 
-  goaccessWebsiteMetricsScriptPart = { hostname, excludeTerms ? [], ... }: ''
-    mkdir -p ${goaccessDir}/${hostname}
-
-    # combine the gzipped and non-gziped logs together
-    ${pkgs.coreutils}/bin/cat \
-      <(${pkgs.findutils}/bin/find . -regextype awk -regex "./${hostname}.access.log.[0-9]+.gz" &&
-          ${pkgs.gzip}/bin/zcat -q ${hostname}.access.log.*.gz) \
-      <(${pkgs.findutils}/bin/find . -regextype awk -regex "./${hostname}.access.log(\.[0-9]+)?" |
-          ${pkgs.findutils}/bin/xargs ${pkgs.coreutils}/bin/cat) |
-
-    # Filter out any logs that match the excludeTerms.
-    ${pkgs.gnugrep}/bin/grep -v \
-      ${concatMapStringsSep " " (e: "-e \"${e}\"") excludeTerms} |
-
-    # Pipe the logs to goaccess
-    ${pkgs.goaccess}/bin/goaccess - \
-      -o ${goaccessDir}/${hostname}/index.html \
+  goaccessCmd = infile: outfile: ''
+    ${pkgs.goaccess}/bin/goaccess ${infile} \
+      -o ${outfile} \
       --ignore-crawlers \
       ${concatMapStringsSep " " (e: "-e \"${e}\"") excludeIPs} \
       --real-os \
       --log-format=COMBINED
   '';
 
-  hostListItem = { hostname, ... }: ''
-    echo "<li><a href=\"/metrics/${hostname}\">${hostname}</a></li>" >> ${goaccessDir}/index.html
+  goaccessWebsiteMetricsForDayScriptPart = infile: hostname: n: ''
+    # Output log for day $DATE - ${toString n}
+    logdatefmt=$(${pkgs.coreutils}/bin/date +%d/%b/%Y -d "$DATE - ${toString n} day")
+    outdatefmt=$(${pkgs.coreutils}/bin/date +%Y-%m-%d -d "$DATE - ${toString n} day")
+    ${pkgs.coreutils}/bin/cat ${infile} |
+      ${pkgs.gnugrep}/bin/grep "\[$logdatefmt" |
+      ${goaccessCmd "-" "${goaccessDir}/${hostname}/days/$outdatefmt.html"}
   '';
+
+  pipeIf = condition: cmd: if condition then "| ${cmd}" else "";
+
+  goaccessWebsiteMetricsScriptPart = { hostname, excludeTerms ? [], ... }: ''
+    mkdir -p ${goaccessDir}/${hostname}/days
+
+    logtmp=$(${pkgs.coreutils}/bin/mktemp)
+    trap "rm -rfv $logtmp" EXIT
+
+    # combine the gzipped and non-gziped logs together
+    ${pkgs.coreutils}/bin/cat \
+      <(${pkgs.findutils}/bin/find . -regextype awk -regex "./${hostname}.access.log.[0-9]+.gz" &&
+          ${pkgs.findutils}/bin/xargs ${pkgs.gzip}/bin/zcat -fq) \
+      <(${pkgs.findutils}/bin/find . -regextype awk -regex "./${hostname}.access.log(\.[0-9]+)?" |
+          ${pkgs.findutils}/bin/xargs ${pkgs.coreutils}/bin/cat) \
+          ${pipeIf (excludeTerms != [])
+            "${pkgs.gnugrep}/bin/grep -v ${concatMapStringsSep " " (e: "-e \"${e}\"") excludeTerms}"} \
+      > $logtmp
+
+    # Run Goaccess for all of the logs that we have.
+    ${goaccessCmd "$logtmp" "${goaccessDir}/${hostname}/index.html"}
+
+    # Run Goaccess for the past week days as well.
+    ${concatMapStringsSep "\n" (goaccessWebsiteMetricsForDayScriptPart "$logtmp" hostname) (lib.range 0 7)}
+
+    # Clean-up days older than a month.
+    # TODO
+  '';
+
+  hostListItem = { hostname, ... }: ''
+    echo "
+        <li>
+          <a href=\"/metrics/${hostname}\">${hostname}</a>
+          (<a href=\"/metrics/${hostname}/days\">Per Day</a>)
+        </li>" >> ${goaccessDir}/index.html
+  '';
+
   makeIndexScriptPart = websites: ''
     echo "<html>"                                > ${goaccessDir}/index.html
     echo "<head><title>Metrics</title></head>"  >> ${goaccessDir}/index.html
