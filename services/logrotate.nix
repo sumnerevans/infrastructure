17a05621990dd137d21c92c050efe84057c57072
Sumner Evans <me@sumnerevans.com>
2021-02-18 07:49:04 -0700

    Use more recent logrotate config

--- services/logrotate.nix
+++ services/logrotate.nix
@@ -1,38 +1,24 @@
 { config, pkgs, ... }: {
   services.logrotate = {
     enable = true;
-    # For when this method gets in to unstable
-    # paths = {
-    #   "/var/log/nginx/*.log /var/spool/nginx/logs/*.log" = {
-    #     user = "nginx";
-    #     group = "nginx";
-    #     extraConfig = ''
-    #       size 25M
-    #       compress
-    #       delaycompress
-    #       create 0644 nginx nginx
-    #       sharedscripts
-    #       postrotate
-    #         /usr/bin/env kill -USR1 `cat /run/nginx/nginx.pid 2>/dev/null` 2>/dev/null || true
-    #       endscript
-    #     '';
-    #   };
-    # };
-
-    extraConfig = ''
-      "/var/log/nginx/*.log" "/var/spool/nginx/logs/*.log" {
-        size 25M
-        missingok
-        rotate 16
-        compress
-        delaycompress
-        notifempty
-        create 0644 nginx nginx
-        sharedscripts
-        postrotate
-          /usr/bin/env kill -USR1 `cat /run/nginx/nginx.pid 2>/dev/null` 2>/dev/null || true
-        endscript
-      }
-    '';
+    paths = {
+      "nginx" = {
+        user = "nginx";
+        group = "nginx";
+        path = "/var/log/nginx/*.log /var/spool/nginx/logs/*.log";
+        extraConfig = ''
+          size 25M
+          missingok
+          compress
+          delaycompress
+          notifempty
+          create 0644 nginx nginx
+          sharedscripts
+          postrotate
+            /usr/bin/env kill -USR1 `cat /run/nginx/nginx.pid 2>/dev/null` 2>/dev/null || true
+          endscript
+        '';
+      };
+    };
   };
 }
