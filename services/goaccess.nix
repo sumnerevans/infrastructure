cf38e9bc1a6cac8f01a19cef4d09e9acef097f9f
Sumner Evans <me@sumnerevans.com>
2021-02-19 10:41:33 -0700

    Improve goaccess

--- services/goaccess.nix
+++ services/goaccess.nix
@@ -4,6 +4,8 @@
   goaccessDir = "/var/www/goaccess";
   excludeIPs = [
     "184.96.89.215"
+    "184.96.97.165"
+    "63.239.147.18"
   ];
 
   goaccessCmd = infile: outfile: ''
@@ -26,31 +28,34 @@
 
   pipeIf = condition: cmd: if condition then "| ${cmd}" else "";
 
-  goaccessWebsiteMetricsScriptPart = { hostname, excludeTerms ? [], ... }: ''
-    mkdir -p ${goaccessDir}/${hostname}/days
+  goaccessWebsiteMetricsScript = { hostname, excludeTerms ? [], ... }:
+    pkgs.writeShellScript "goaccess-${hostname}" ''
+      set -xef
+      cd /var/log/nginx
+      mkdir -p ${goaccessDir}/${hostname}/days
 
-    logtmp=$(${pkgs.coreutils}/bin/mktemp)
-    trap "rm -rfv $logtmp" EXIT
+      logtmp=$(${pkgs.coreutils}/bin/mktemp)
+      trap "rm -rfv $logtmp" EXIT
 
-    # combine the gzipped and non-gziped logs together
-    ${pkgs.coreutils}/bin/cat \
-      <(${pkgs.findutils}/bin/find . -regextype awk -regex "./${hostname}.access.log.[0-9]+.gz" &&
-          ${pkgs.findutils}/bin/xargs ${pkgs.gzip}/bin/zcat -fq) \
-      <(${pkgs.findutils}/bin/find . -regextype awk -regex "./${hostname}.access.log(\.[0-9]+)?" |
-          ${pkgs.findutils}/bin/xargs ${pkgs.coreutils}/bin/cat) \
-          ${pipeIf (excludeTerms != [])
-            "${pkgs.gnugrep}/bin/grep -v ${concatMapStringsSep " " (e: "-e \"${e}\"") excludeTerms}"} \
-      > $logtmp
+      # Combine the gzipped and non-gziped logs together
+      ${pkgs.coreutils}/bin/cat \
+        <(${pkgs.findutils}/bin/find . -regextype awk -regex "./${hostname}.access.log.[0-9]+.gz" |
+            ${pkgs.findutils}/bin/xargs ${pkgs.gzip}/bin/zcat -fq) \
+        <(${pkgs.findutils}/bin/find . -regextype awk -regex "./${hostname}.access.log(\.[0-9]+)?" |
+            ${pkgs.findutils}/bin/xargs ${pkgs.coreutils}/bin/cat) \
+            ${pipeIf (excludeTerms != [])
+      "${pkgs.gnugrep}/bin/grep -v ${concatMapStringsSep " " (e: "-e \"${e}\"") excludeTerms}"} \
+        > $logtmp
 
-    # Run Goaccess for all of the logs that we have.
-    ${goaccessCmd "$logtmp" "${goaccessDir}/${hostname}/index.html"}
+      # Run Goaccess for all of the logs that we have.
+      ${goaccessCmd "$logtmp" "${goaccessDir}/${hostname}/index.html"}
 
-    # Run Goaccess for the past week days as well.
-    ${concatMapStringsSep "\n" (goaccessWebsiteMetricsForDayScriptPart "$logtmp" hostname) (lib.range 0 7)}
+      # Run Goaccess for the past week days as well.
+      ${concatMapStringsSep "\n" (goaccessWebsiteMetricsForDayScriptPart "$logtmp" hostname) (lib.range 0 7)}
 
-    # Clean-up days older than a month.
-    # TODO
-  '';
+      # Clean-up days older than a month.
+      # TODO
+    '';
 
   hostListItem = { hostname, ... }: ''
     echo "
@@ -60,7 +65,11 @@
         </li>" >> ${goaccessDir}/index.html
   '';
 
-  makeIndexScriptPart = websites: ''
+  goaccessScript = websites: pkgs.writeShellScript "goaccess" ''
+    set -xe
+    cd /var/log/nginx
+    ${pkgs.coreutils}/bin/mkdir -p ${goaccessDir}
+
     echo "<html>"                                > ${goaccessDir}/index.html
     echo "<head><title>Metrics</title></head>"  >> ${goaccessDir}/index.html
     echo "<body>"                               >> ${goaccessDir}/index.html
@@ -71,16 +80,6 @@
 
     echo "</ul>"                                >> ${goaccessDir}/index.html
     echo "</body>"                              >> ${goaccessDir}/index.html
-  '';
-
-  goaccessScript = websites: pkgs.writeShellScript "goaccess" ''
-    set -xe
-    ${pkgs.coreutils}/bin/mkdir -p ${goaccessDir}
-    cd /var/log/nginx
-
-    ${concatMapStringsSep "\n" goaccessWebsiteMetricsScriptPart websites}
-
-    ${makeIndexScriptPart websites}
   '';
 in
 {
@@ -117,17 +116,32 @@ in
     };
 
   config = mkIf (cfg != {}) {
-    systemd.services.goaccess = {
-      description = "Goaccess Web log report.";
-      wantedBy = [ "multi-user.target" ];
-      after = [ "network-online.target" ];
-      serviceConfig = {
-        User = "root";
-        ExecStart = (goaccessScript cfg.websites);
-        Restart = "always";
-        RestartSec = 600;
+    systemd.services = let
+      mkGoaccessService = website: {
+        name = "goaccess-${website.hostname}";
+        value = {
+          description = "Goaccess web log report.";
+          wantedBy = [ "multi-user.target" ];
+          serviceConfig = {
+            User = "root";
+            ExecStart = "${goaccessWebsiteMetricsScript website}";
+            Restart = "always";
+            RestartSec = 600;
+          };
+        };
       };
-    };
+    in
+      listToAttrs (map mkGoaccessService cfg.websites) // {
+        goaccess-index = {
+          description = "Generate Goaccess index.";
+          wantedBy = [ "multi-user.target" ];
+          serviceConfig = {
+            User = "root";
+            ExecStart = "${goaccessScript cfg.websites}";
+            Type = "oneshot";
+          };
+        };
+      };
 
     # Set up nginx to forward requests properly.
     services.nginx.virtualHosts."${hostnameDomain}" = {
